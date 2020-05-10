e955581cacab15f62c855948bd08fa5d0f972cf0
AdamWier <38018787+AdamWier@users.noreply.github.com>
2020-05-10 16:43:14 +0200

    Added basic url getter but front not updating

--- client/main.js
+++ client/main.js
@@ -1,8 +1,22 @@
 import { Template } from 'meteor/templating';
-
+import firebase from 'firebase/app';
+import 'firebase/storage';
 import './main.html';
 
-Template.links.helpers({
-  preProductionLink: 'http://google.com',
+const firebaseConfig = {
+  apiKey: Meteor.settings.public.FIREBASE_API_KEY,
+  storageBucket: Meteor.settings.public.FIREBASE_STORAGE_BUCKET,
+};
+
+firebase.initializeApp(firebaseConfig);
+var storage = firebase.storage();
+
+storage.ref('preProd/mega-fitness-app-preprod.apk').getDownloadURL().then(preProductionLink => {
+  Template.links.helpers({
+  preProductionLink: preProductionLink,
   productionLink: 'http://freecodecamp.com'
+})})
+
+Template.links.onCreated(() => {
+  this.preProductionLink = new ReactiveVar('http://freecodecamp.com')
 })
