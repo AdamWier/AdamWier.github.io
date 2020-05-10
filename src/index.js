c6f4dbd63f3e6340249f3b9c5f54d1315e14b82a
AdamWier <38018787+AdamWier@users.noreply.github.com>
2020-05-10 17:09:34 +0200

    Basic website

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey,
-  storageBucket,
+  apiKey: "AIzaSyAGDFX6-GcfeCOyV4E-5Yumg7PpEZy8Ce0",
+  storageBucket: "myapplication-8a6a6.appspot.com",
 };
 
 firebase.initializeApp(firebaseConfig);
 var storage = firebase.storage();
 
 storage.ref('preProd/mega-fitness-app-preprod.apk').getDownloadURL().then(preProductionLink => {
-  console.log(preProductionLink)
+  document.getElementById('preproduction-link').setAttribute('href', preProductionLink)
 })
