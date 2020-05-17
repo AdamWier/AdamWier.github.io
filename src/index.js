a221db6b4d82e53b009a66cdbc2b62893745fe0d
AdamWier <38018787+AdamWier@users.noreply.github.com>
2020-05-17 22:42:13 +0200

    Reactived prod button, added loaders and update date

--- src/index.js
+++ src/index.js
@@ -8,17 +8,23 @@ import './badge/badge-gh.css';
 const initToasts = () => {
   var elems = document.querySelectorAll('.tooltipped');
   M.Tooltip.init(elems);
-  // document.getElementById('prod-link').addEventListener('click', toast);
+  document.getElementById('prod-link').addEventListener('click', toast);
   document.getElementById('master-link').addEventListener('click', toast);
 }
 
 const initLink = (buildName) => {
   var storage = firebase.storage();
-  storage.ref(`releases/mega-fitness-app-${buildName}.apk`)
-    .getDownloadURL()
+  const ref = storage.ref(`releases/mega-fitness-app-${buildName}.apk`)
+  ref.getDownloadURL()
     .then(link => {
       document.getElementById(`${buildName}-link`)
         .setAttribute('href', link)
+      ref.getMetadata().then(metadata => {
+        const date = new Date(metadata.updated).toLocaleString();
+        document.getElementById(`${buildName}-updated-at`).innerText = date;
+        document.getElementById(`${buildName}-preloader`).style.display = 'none';
+        document.getElementById(`${buildName}-button`).style.display = 'block';
+      })
   })
 }
 
@@ -35,6 +41,6 @@ firebase.initializeApp(firebaseConfig);
 document.addEventListener('DOMContentLoaded', function() {
   initToasts();
   initLink('master');
-  // initLink('prod');
+  initLink('prod');
   initGitBadge();
 });
